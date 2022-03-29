5e53b945ff2930226e301ce1c23a567865f28b43
Кирилл <ma-kir.ru@yandex.ru>
2022-03-29 20:29:40 +0200

    bugfix temperature param in function

--- server.R
+++ server.R
@@ -1,6 +1,4 @@
 library(shiny)
-
-# install.packages("ggplot2")
 library(readxl)
 
 df = read_excel('models.xlsx')
@@ -10,12 +8,13 @@ ordonnees <- as.numeric(df$intercept)
 heures <- as.numeric(df$hour)
 
 prevision <- function(heure,temperature) {
+  temperature = temperature + 273.15
   pente = pentes[heure+1]
   ordonnee = ordonnees[heure+1]
   return(pente*temperature + ordonnee)
 }
 
-# Création du graphique
+
 shinyServer(function(input, output) {
   output$graph <- renderPlot({plot(y = prevision(heures, input$Temp),x = heures,type = 'l')})
 })
